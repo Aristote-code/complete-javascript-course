d4688e7318a00fbb5610952db1593ae39e0cd991
aristote-code <gahimaaristote1@gmail.com>
2023-11-01 19:02:09 +0200

    update 06-Modal/starter/index.html and script.js

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const btn = document.querySelectorAll('.show-modal');
+const close = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+const modalReveal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hid = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', modalReveal)
+}
+
+close.addEventListener('click', hid);
+
+overlay.addEventListener('click', hid);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !(modal.classList.contains('hidden'))) {
+        hid();
+    }
+})
